5ee43ab2c8d8c4629af909450c43fc6b542c65fe
DiyorbekkAbullayev <github1805@gmail.com>
2022-11-27 08:46:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,470 @@
+// var a = 14;
+// var a = 15;
+// A butun son berilgan. Jumlani rostlikka tekshiring: A son musbat
+// if(a == 14){
+//     console.log('siz oqishga kiridngiz');
+// }else{
+//     console.log('siz ota olmadingiz!');
+// }
+
+// vaz 2
+// A butun son berilgan. Junlani rostlikka tekshiring: A juft son
+// var one = prompt('son kiriting!')
+
+// if(one % 2 == 0){
+//     console.log('bu juft son');
+// }
+// else{
+//     console.log('bu toq son!');
+// }
+
+// vaz3
+
+// var a = 18;
+// var b = 0;
+// Ikkita butun son berilgan. Jumlani rostlikka tekshiring. M-s a > 0 && b > 0
+// if(a > 0 && b > 0 ){
+//     console.log('true');
+// }
+// else{
+//     console.log('false');
+// }
+
+// vaz4
+// 3 ta son berilgan a,b,c. a bdan katta b c dan katta rostlikka tekshiring
+// var a = 18;
+// var b = 5;
+// var c = 8;
+// if (a > b && b > c) {
+//   console.log("a son b dan katta");
+//   console.log("b son c dan katta");
+
+// } else {
+//   console.log("qayerdadir xato bor!");
+// }
+
+// vaz 5
+// a, b, c, son berilgan. B son a va c sonlarni orasida yotadi.
+// var a = 11;
+// var b = 12;
+// var c = 15;
+
+// if(a<b<c){
+//     console.log('B son a va c sonlarni orasida yotadi.');
+// }else{
+//     console.log('formula xato');
+// }
+
+// vaz 6 ;
+// a, b sonlar berilgan sonlarni toq songa tekshiring.
+// var a = 11;
+// var b = 9;
+
+// if (a % 2 == 0 && b % 2 == 0) {
+//     console.log('juft son');
+// }else{
+//     console.log('toq son');
+// }
+
+// vaz 7
+// hafta kunlari berilgan raqamga qarab hafta kunlarini nomini chiqaring
+// var a = prompt('son yozing!')
+// if(a==1){
+//     console.log('duwanba');
+// }else if(a==2){
+//     console.log('sewanba');
+// }else if(a==3){
+//     console.log('chorwanba');
+// }else if(a==4){
+//     console.log('Paywanba');
+// }else if(a==5){
+//     console.log('juma');
+// }else if(a==6){
+//     console.log('wanba');
+// }else if(a==7){
+//     console.log('yakwanba');
+// }else{
+//     console.log('bunday xafta kuni yoq!');
+// }
+
+// vaz8
+// Telefon nomer kodi orqali qaysi kompaniya ekanligini tekshiring
+// var a = prompt("Telefon nomer kodini kiriting!");
+
+// if (a == 93 || a == 94) {
+//   console.log("Ucell");
+// } else if (a == 90 || a == 91) {
+//   console.log("beeline");
+// } else if (a == 99) {
+//   console.log("uzmobile");
+// } else if (a == 97 || a == 88 || a == 33) {
+//   console.log("mobileuz");
+// }else{
+//     console.log('bunday kod yoq');
+// }
+
+// vaz 9
+// a,b,c sonlar berilgan kamida ikkitasi musbat berilgan
+// var a = 1;
+// var b = -1;
+// var c = -3;
+
+// if (a > 0 && b > 0 && c < 0) {
+//   console.log("amal togri");
+// } else {
+//   console.log("notogri");
+// }
+
+// vaz10
+// a,b,c,d sonlar berilgan barchasi musbat
+// var a = 1;
+// var b = 2;
+// var c = 3;
+// var d = 3;
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//   console.log("barchasi musbat son!");
+// }else{
+//     console.log('barchasi manfiy!');
+// }
+
+// vaz 11
+// Butun son berilgan agar son musbat bo'lsa unda sonni bittaga oshiring aks xolda 1taga kamaytiring
+
+// var a= prompt("son kiriting!")
+// if(a>0){
+//     a++
+//     console.log(a);
+// }else{
+//     a--
+//     console.log(a);
+// }
+
+// vaz 12;
+// Butun son berilgan agar son musbat bo'lsa sonni 3 ga oshiring manfiy bo'lsa unda 2ga kamaytiring
+// var a= +prompt("son kiriting!")
+// if(a>0){
+//     a+=3;
+//     console.log(a);
+// }else{
+//     a-=2;
+//     console.log(a);
+// }
+
+// vaz 13
+// Ikkita butun son berilgan kattasin aniqlovchi dastur tuzing
+// var a = 4;
+// var b = 19;
+// if (a>b){
+//     console.log('a b dan katta');
+// }else{
+//     console.log('b a dan katta');
+
+// }
+
+// vaz 14
+// 3 ta butun son berilgan eng kattasini aniqlovchi dastur tuzing
+// var a = 10;
+// var b = 2;
+// var c = 3;
+// if (a >b<c){
+//     console.log('000');
+// }else{
+//     console.log('gdfg');
+// }
+
+// vaz 15
+// 3 ta butun son berilgan eng kattasini aniqlovchi dastur tuzing
+//  var a = 10;
+//  var b = 2;
+//  var c = 5;
+//  if (a >b<c){
+//      console.log('000');
+//  }else{
+//      console.log('gdfg');
+//  }
+
+// // vaz 16
+// var a=prompt('jins kirit!')
+// if(a=='erkak'){
+//     console.log('erkak');
+// }else if(a=='ayol'){
+//     console.log('ayol');
+// }
+// var a1=document.getElementById('a1')
+// var a2=document.getElementById('a2')
+// if(a=='erkak'){
+//     a1.style.display='block';
+// }else{
+//     a1.style.display='none';
+
+// }
+// if(a=='ayol'){
+//     a2.style.display='block';
+// }else{
+//     a2.style.display='none';
+
+// }
+// vaz 17
+// Bahoga qarab stependiyani aniqlovchi dastur tuzing
+
+// var a=prompt('stipendiya kiriting!')
+// if(a==5){
+//     console.log('300ming');
+// }else if(a==4){
+//     console.log('200ming');
+// }else if(a==2){
+//     console.log('stipemdiya yoq!');
+// }
+
+// vaz 18
+// Butun son berilgan shu songa qarab toq musbat, toq manfiy, juft musbat, juft manfiy deb chiqaruvchi dastur tuzing
+
+// var a=prompt('son yozing!')
+// if(a % 2==0){
+// console.log('juft');
+// }else {
+//     console.log('toq');
+// }
+
+// if(a<0){
+//     console.log('manfiy');
+// } if(a>0){
+//     console.log('musbat');
+// }
+
+// vaz 19
+// Yilga qarab yoshin aniqlovchi dastur tuzing
+// var a= +prompt('yilizni kiriting!')
+
+// console.log('sizni yoshiz ' + (2022-a));
+
+// vaz 20
+// 3 ta butun son berilgan jumlani rostlikkar tekshiring. A <= B <= C
+
+// var a = 20;
+// var b = 20;
+// var c = 22;
+
+// if (a <= b <= c) {
+//   console.log("true");
+// }else{
+//     console.log('false');
+// }
+
+// vaz 21
+// ikki butun son berilgan ikki sondan faqat 1tasi toq son hisoblanadi
+
+// var a = 20;
+// var c = 21;
+// if (a % 2 == 0) {
+//   console.log("a juft");
+// }else{
+//     console.log('a toq');
+// }
+// if (c % 2 == 0) {
+//     console.log("c juft");
+//   }else{
+//       console.log('c toq');
+//   }
+
+// vaz 22
+// a,b,c sonlar berilgan har biri musbat son
+// var num = [2, 3, 4];
+// for (var i = 0; i < num.length; i++) {
+//   if (num[i] > 0) {
+//     console.log("musbat");
+//   }
+// }
+
+// vaz 23
+// a,b,c sonlardan biri musbat son
+// var num = [-2, -3, 4];
+// for (var i = 0; i < num.length; i++) {
+//   if (num[i] > 0) {
+//     console.log(num[i]);
+//   }
+// }
+
+// vaz 24
+// // 3 xonali son berilgan har bir raqamlari har xil
+// var a= 234;
+
+// for loop vazifalar
+
+// vaz 1
+// k va n butun son berilgan. K ni n marta chiqaruvchi dastur tuzing
+
+// var a=1;
+// var b=2;
+// for(var i =0; i<b; i++){
+//     alert(a)
+// }
+
+// vaz 2
+// a, b sonlar beilgan. A b dan katta a va b orasidagi butun sonlarni chiqaruvchi dastur tuzing. A va B ni ozi ham chiqsin
+// var a = 10;
+// for (var b = 5; b <= a; b++) {
+//   alert(b)
+// }
+
+// vaz 3
+// a,b sonlar berilgan a b dan katta a va b orasidagi barcha butun sonlarni kamayish tartibida chiqaring;
+// var b = 5;
+// for (var a = 10; a>b; a--) {
+//   alert(a)
+// }
+
+// vaz 4
+// 1kg konfet 10000so'm berilgan. 1 kgdan 10 kggacha barchasin narxini chiqaruvchi dastur tuzing
+// var a= 10000;
+// var b= 10;
+// for(var i=0; i<=b; i++){
+//   alert(i*a +' som')
+// }
+
+// vaz 5
+// 1kg konfet 10000so'm. 0,1 kg dan 1 kg gacha bo'lgan konfet narxin toping
+// var a = 10000;
+// var b = 1;
+// for(var i=0.1; i<=b; i+=0.1){
+//   alert(i*a +' som')
+
+// }
+// vaz6
+// 1kg konfet 10000so'm. 1,2kg, 1,4, 1,6, 1,8, 2kg konfetlarni narxini toping
+// var a= 10000;
+// var b= 2;
+// for(var i=1.2; i<=b; i+=0.2){
+//   alert(i*a +' som')
+// }
+
+// vaz 7
+// a,b sonlar berilgan. A dan b gacha barcha  butun sonlarni yig'indisin topuvchi dastur yozing
+// var a = 5;
+// var b = 10;
+
+// function getNum(a,b){
+//     let sum=0;
+//     for(i=a;i<b;i++){
+//         sum+=i;
+//     }
+//     console.log(sum);
+// }
+// getNum(a,b)
+
+// vaz 8
+// a,b sonlar berilgan. A dan b gacha barcha butun sonlarni ko'paytmasin topuvchi dastur yozing
+// var a=3;
+// var b=7;
+// var sum=1;
+// for(i=a; i<b; i++){
+//     sum*=i;
+// console.log(sum);
+// console.log(i);
+// }
+
+// vaz 9;
+// a,b sonlar berilgan a dan b gacha bo'lgan sonlarni kvadratlarini yig'indisin topuvchi dastur toping
+// yechim1
+// var a = 7;
+// var c = a * (a + 1) * (2 * a + 1) / 6;
+// console.log(c);
+// yechim2
+// function sumN(a,b){
+//     var sum=0;
+//     let c=0;
+//     let arr=[];
+//     for(var i=a; i<=b; i++){
+//         c=i**2;
+//         sum+=c;
+//        arr.push(c);
+//        console.log(arr);
+//     var qq=0;
+
+//        for(var w=0; w<arr.length; w++){
+//         qq+=arr[w];
+//         console.log(qq);
+//        }
+
+//     }
+// }
+// sumN(1,7)
+
+// vaz10
+// n soni berilgan quydagi yig'indini hisoblang. S = 1/1 + 1/2 + …. + 1/n
+// var n=5;
+// var sum=0;
+// for(var i=1;i<n; i++){
+//     sum+=1/i;
+//     console.log(sum);
+// }        
+
+
+// vaz 11
+
+// n soni berilgan shu soni ichidan toqlarin ko'ruvchi dastur yozing
+
+// var n = 10;
+// for(var i=0; i<= n; i++){
+//     if(i%2==0){
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// vaz 12
+// n soni berilgan shu sonlarni ichidan juftlarini koruvchi dastur yozing
+
+// var n = 10;
+// for(var i=0; i<= n; i++){
+//     if(i%2==0){
+//         console.log(i);
+
+//     }
+// }
+
+// vaz 13
+// n soni berilgan shu son ichidan faqat murakkab sonlarni aniqlovchi dastur yozing
+
+// function sum(n) {
+//     for (var i = 0; i < n; i++) {
+//         if (i % 2 == 0 && i % i == 1) {
+//             console.log(i);
+//         }
+//     }
+// }
+// sum(10)
+
+// vaz14
+// n soni berilgan shu sonlarni ichidan jufat va toq sonlarni kvadratlarini chiqaring
+
+// function son(n) {
+//     var a = 0;
+//     var juft=[];
+//     var toq=[];
+//     for (var i = 0; i <= n; i++) {
+//         if (i % 2 == 0) {
+//             a = i ** 2;
+//             juft.push(a);
+//             console.log(juft);
+//         }
+//         else {
+//             a=i**2;
+//             toq.push(a);
+//             console.log(toq);
+//         }
+
+//     }
+// }
+// son(10);
+
+// vaz15
+// n son berilgan n sonidan kamayish tartibida sonlar chiqarilsin
+
+// function son(n){
+//     for(var i = n; i>0 ;i--){   
+//         console.log(i);
+//      }
+// }
+// son(5)
